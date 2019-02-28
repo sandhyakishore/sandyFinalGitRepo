ccc4f6a93a2bbe1b75cd20f730982028495a5e5e
sandhyakishore <sandysiriginedi@gmail.com>
2019-02-28 12:47:53 +0530

    first commit

---
+++ SandyFinalGit/src/main/java/SandyFinalGit/SandyFinalGit/App.java
@@ -0,0 +1,13 @@
+package SandyFinalGit.SandyFinalGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ SandyFinalGit/src/test/java/SandyFinalGit/SandyFinalGit/NewSample.java
@@ -0,0 +1,12 @@
+ package SandyFinalGit.SandyFinalGit;
+
+public class NewSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my first commit");
+
+	}
+
+}
